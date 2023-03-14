d091b08a6e472d9b52723344fbff34cb9e1a422b
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2023-03-14 15:43:48 +0800

    feat: add search bar & categories

---
+++ lib/Components/Category.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatefulWidget {
+  final String imagePath;
+  final String categoryName;
+
+  const Category(
+      {Key? key, required this.imagePath, required this.categoryName})
+      : super(key: key);
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 100,
+        width: 100,
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.green,
+                    ),
+                    child: Image.asset(
+                      widget.imagePath,
+                      height: 50,
+                      width: 50,
+                    ))),
+            Text(widget.categoryName)
+          ],
+        ));
+  }
+}
--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:recloset/Components/Categories.dart';
+import 'package:recloset/Components/Category.dart';
+import 'package:recloset/Components/SearchBar.dart';
+
+class CategoryType {
+  String image;
+  String categoryName;
+
+  CategoryType(this.image, this.categoryName);
+}
 
 class Home extends StatelessWidget {
+  final List<CategoryType> categories = [
+    CategoryType("assets/shirt.png", "Tops"),
+    CategoryType("assets/shirt.png", "Bottoms"),
+    CategoryType("assets/shirt.png", "Dresses"),
+    CategoryType("assets/shirt.png", "Outerwear"),
+    CategoryType("assets/shirt.png", "Activewear"),
+    CategoryType("assets/shirt.png", "Accessories"),
+    CategoryType("assets/shirt.png", "Others"),
+  ];
+
+  Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Display all the items here',
-    );
+    return Scaffold(
+        body: ListView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        SearchBar(
+          hintText: 'Search',
+          onChanged: (value) {
+            // Do something with the search query
+            print('Search query: $value');
+          },
+        ),
+        Categories(categories: categories)
+      ],
+    ));
   }
 }

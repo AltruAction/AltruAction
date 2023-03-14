99b3a741c2c70efc29ff968048a1087724e2cbec
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2023-03-14 18:05:06 +0800

    feat: modularise out bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'MyHomePage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 

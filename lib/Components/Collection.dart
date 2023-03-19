7e90b02e5d25f99c030a3dc12d6f240a38b61fda
Cedric Ong <cedricongjh@gmail.com>
2023-03-20 03:15:58 +0800

    add option to hide title in collection

--- lib/Components/Collection.dart
+++ lib/Components/Collection.dart
@@ -21,10 +21,12 @@ class ItemCardData {
 class Collection extends StatefulWidget {
   final String title;
   final List<ItemCardData> items;
+  final bool showTitle;
   const Collection({
     Key? key,
     required this.title,
     required this.items,
+    this.showTitle = true,
   }) : super(key: key);
 
   @override
@@ -37,7 +39,7 @@ class _CollectionState extends State<Collection> {
     return Container(
         padding: const EdgeInsets.all(10),
         child: Column(children: [
-          Row(
+          if (widget.showTitle) ...[Row(
             children: [
               Align(
                   alignment: Alignment.topLeft,
@@ -64,7 +66,7 @@ class _CollectionState extends State<Collection> {
                         decoration: TextDecoration.underline),
                   ))
             ],
-          ),
+          )],
           SizedBox(
               height: 200,
               child: ListView(

b3ceb532ffdca5426f3ae6a2d8bdc377c023cf2c
Cedric Ong <cedricongjh@gmail.com>
2023-03-20 03:16:25 +0800

    create profile page

--- lib/Pages/Profile.dart
+++ lib/Pages/Profile.dart
@@ -1,42 +1,128 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
+import 'package:recloset/Components/ProfilePageHeader.dart';
+import 'package:recloset/Components/ProfilePageName.dart';
+import 'package:recloset/Components/ProfilePicture.dart';
 import 'package:recloset/Pages/login.dart';
 
+import '../Components/ProfilePageItemList.dart';
 import '../app_state.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<Profile> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (FirebaseAuth.instance.currentUser == null) {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const Login(),
+        ));
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Consumer<ApplicationState>(
-            builder: (context, appState, _) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (appState.loggedIn) ...[
-                  const Text('I am logged in'),
-                  TextButton(onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  }, child: const Text('logout'))
-                ],
-                if (!appState.loggedIn) ... [
-                  const Text('I am not logged in'),
-                  TextButton(onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const Login(),
-              )); }, child: const Text('login'))
-                ]
-              ],
+      backgroundColor: Colors.white,
+      body: Consumer<ApplicationState>(
+              builder: (context, appState, _) {
+                if (appState.user == null) {
+                  return const CircularProgressIndicator();
+                }
+                return ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      clipBehavior: Clip.none,
+                      children: [
+                        const ProfilePageHeader(),
+                        Positioned(top: 100, child: ProfilePicture(imagePath: appState.user?.photoURL ?? ""))
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(top: 75),
+                      child: ProfilePageName(displayName: appState.user?.displayName ?? "", 
+                                              bio: "test bio")
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10, top: 15, bottom: 25),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(
+                          25.0,
+                        ),
+                      ),
+                      child:
+                        TabBar(
+                          unselectedLabelColor: Colors.grey,
+                          indicator: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              25.0,
+                            ),
+                            color: Colors.white,
+                            boxShadow: [BoxShadow(
+                              color: Colors.grey.shade600,
+                              spreadRadius: 0.9,
+                              blurRadius: 10,
+                              blurStyle: BlurStyle.inner
+                            )]
+                          ),
+                          labelColor: Colors.green,
+                          tabs: const [
+                            Tab(
+                              text: "Listings"
+                            ),
+                            Tab(
+                              text: "Liked"
+                            ),
+                            Tab(
+                              text: "Statistics"
+                            ),
+                          ],
+                          controller: _tabController,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                        )
+                    ),
+                    SizedBox(
+                      height: 400,
+                      child: Center(child: TabBarView(
+                        controller: _tabController,
+                        children: const [
+                          Center(
+                            child: ProfilePageItemList()
+                          ),
+                          Center(
+                            child: ProfilePageItemList()
+                          ),
+                          Center(
+                            child: Text(
+                              'Statistics',
+                            ),
+                          ),
+                        ],
+                      ),
+                      )
+                    ),
+                  ]
+                );
+              }
             ),
-          ),
-          ],
-        ),
-      ),
-    );
+      );
   }
 }

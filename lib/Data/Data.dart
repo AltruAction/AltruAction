47b3790df054527a89ef54a8dfc4f663ed9fdfb0
tau-bar <taufiq.b.ar@gmail.com>
2023-03-21 01:56:54 +0800

    feat: change item card images to placeholder

--- lib/Data/Data.dart
+++ lib/Data/Data.dart
@@ -7,26 +7,26 @@ class Store {
 
 class DummyData {
   static List<ItemCardData> itemCardData = [
-    ItemCardData(0, "White shirt", "assets/shirt.png", 10,
+    ItemCardData(0, "White shirt", "assets/placeholder.jpg", 10,
         ItemCondition.brandNew, [ItemDealOption.delivery], ItemCategory.tops),
-    ItemCardData(1, "Blue shirt", "assets/shirt.png", 5,
+    ItemCardData(1, "Blue shirt", "assets/placeholder.jpg", 5,
         ItemCondition.heavilyUsed, [ItemDealOption.meetup], ItemCategory.tops),
     ItemCardData(
         2,
         "Green shirt",
-        "assets/shirt.png",
+        "assets/placeholder.jpg",
         7,
         ItemCondition.lightlyUsed,
         [ItemDealOption.meetup, ItemDealOption.delivery],
         ItemCategory.tops),
-    ItemCardData(3, "Yellow shirt", "assets/shirt.png", 4,
+    ItemCardData(3, "Yellow shirt", "assets/placeholder.jpg", 4,
         ItemCondition.likeNew, [ItemDealOption.delivery], ItemCategory.tops),
-    ItemCardData(4, "Orange shirt", "assets/shirt.png", 9,
+    ItemCardData(4, "Orange shirt", "assets/placeholder.jpg", 9,
         ItemCondition.wellUsed, [ItemDealOption.meetup], ItemCategory.tops),
     ItemCardData(
         5,
         "Purple shirt",
-        "assets/shirt.png",
+        "assets/placeholder.jpg",
         2,
         ItemCondition.brandNew,
         [ItemDealOption.meetup, ItemDealOption.delivery],

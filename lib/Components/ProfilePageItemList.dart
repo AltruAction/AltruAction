2f4e21689bc49e2eda35b3a050b2432eef5b3245
Cedric Ong <cedricongjh@gmail.com>
2023-03-25 16:47:25 +0800

    update profile page item list with new item card types

--- lib/Components/ProfilePageItemList.dart
+++ lib/Components/ProfilePageItemList.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:recloset/Components/Collection.dart';
 
+import '../Types/CommonTypes.dart';
+
 class ProfilePageItemList extends StatefulWidget {
   const ProfilePageItemList({Key? key}) : super(key: key);
 
@@ -11,12 +13,31 @@ class ProfilePageItemList extends StatefulWidget {
 class _ProfilePageItemListState extends State<ProfilePageItemList> {
 
   final List<ItemCardData> dummyData = [
-    ItemCardData(0, "White shirt", "assets/shirt.png", 10),
-    ItemCardData(1, "Blue shirt", "assets/shirt.png", 5),
-    ItemCardData(2, "Green shirt", "assets/shirt.png", 7),
-    ItemCardData(3, "Yellow shirt", "assets/shirt.png", 4),
-    ItemCardData(4, "Orange shirt", "assets/shirt.png", 9),
-    ItemCardData(5, "Purple shirt", "assets/shirt.png", 2),
+
+    ItemCardData("0", "White shirt", "assets/placeholder.jpg", 10,
+        ItemCondition.brandNew, [ItemDealOption.delivery], ItemCategory.tops),
+    ItemCardData("1", "Blue shirt", "assets/placeholder.jpg", 5,
+        ItemCondition.heavilyUsed, [ItemDealOption.meetup], ItemCategory.tops),
+    ItemCardData(
+        "2",
+        "Green shirt",
+        "assets/placeholder.jpg",
+        7,
+        ItemCondition.lightlyUsed,
+        [ItemDealOption.meetup, ItemDealOption.delivery],
+        ItemCategory.tops),
+    ItemCardData("3", "Yellow shirt", "assets/placeholder.jpg", 4,
+        ItemCondition.likeNew, [ItemDealOption.delivery], ItemCategory.tops),
+    ItemCardData("4", "Orange shirt", "assets/placeholder.jpg", 9,
+        ItemCondition.wellUsed, [ItemDealOption.meetup], ItemCategory.tops),
+    ItemCardData(
+        "5",
+        "Purple shirt",
+        "assets/placeholder.jpg",
+        2,
+        ItemCondition.brandNew,
+        [ItemDealOption.meetup, ItemDealOption.delivery],
+        ItemCategory.tops),
   ];
 
   @override

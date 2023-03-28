7c2b5664722dbb68d6335aec2f9acb015a034a1d
Cedric Ong <cedricongjh@gmail.com>
2023-03-28 15:58:58 +0800

    update profile page item list to fetch data

--- lib/Components/ProfilePageItemList.dart
+++ lib/Components/ProfilePageItemList.dart
@@ -1,47 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:recloset/Components/Collection.dart';
+import 'package:recloset/Services/ItemService.dart';
 
 import '../Types/CommonTypes.dart';
 
 class ProfilePageItemList extends StatefulWidget {
-  const ProfilePageItemList({Key? key}) : super(key: key);
+  final  List<String> ids;
+  const ProfilePageItemList({Key? key, required this.ids}) : super(key: key);
 
   @override
   State<ProfilePageItemList> createState() => _ProfilePageItemListState();
 }
 
 class _ProfilePageItemListState extends State<ProfilePageItemList> {
+  List<ItemCardData> _items = [];
+  List<ItemCardData> _displayedItems = [];
 
-  final List<ItemCardData> dummyData = [
+  @override
+  void initState() {
+    super.initState();
+    // Retrieve item records from Firestore
+    getData();
+  }
 
-    ItemCardData("0", "White shirt", "assets/placeholder.jpg", 10,
-        ItemCondition.brandNew, [ItemDealOption.delivery], ItemCategory.tops),
-    ItemCardData("1", "Blue shirt", "assets/placeholder.jpg", 5,
-        ItemCondition.heavilyUsed, [ItemDealOption.meetup], ItemCategory.tops),
-    ItemCardData(
-        "2",
-        "Green shirt",
-        "assets/placeholder.jpg",
-        7,
-        ItemCondition.lightlyUsed,
-        [ItemDealOption.meetup, ItemDealOption.delivery],
-        ItemCategory.tops),
-    ItemCardData("3", "Yellow shirt", "assets/placeholder.jpg", 4,
-        ItemCondition.likeNew, [ItemDealOption.delivery], ItemCategory.tops),
-    ItemCardData("4", "Orange shirt", "assets/placeholder.jpg", 9,
-        ItemCondition.wellUsed, [ItemDealOption.meetup], ItemCategory.tops),
-    ItemCardData(
-        "5",
-        "Purple shirt",
-        "assets/placeholder.jpg",
-        2,
-        ItemCondition.brandNew,
-        [ItemDealOption.meetup, ItemDealOption.delivery],
-        ItemCategory.tops),
-  ];
+  void getData() async {
+    var items = await ItemService().getItems();
+    setState(() {
+      _items = items?.values.toList() ?? [];
+      _displayedItems = _items.where((item) => widget.ids.contains(item.id) ).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Collection(title: "", items: dummyData, showTitle: false);
+    return Collection(title: "", items: _displayedItems, showTitle: false);
   }
 }

7a9d666fe1aeef768ab882f271c205fd945de387
Cedric Ong <cedricongjh@gmail.com>
2023-03-28 14:58:12 +0800

    add update liked item to user service

--- lib/Services/UserService.dart
+++ lib/Services/UserService.dart
@@ -4,6 +4,7 @@ import 'package:recloset/Types/UserTypes.dart';
 
 class UserService {
   static var userDb = FirebaseFirestore.instance.collection("users");
+  static var itemDb = FirebaseFirestore.instance.collection("items");
 
   static Map<String, dynamic> toFirestore(UserState user) {
     return {
@@ -33,7 +34,6 @@ class UserService {
     UserState? newUser =
         UserState(uuid, 0, [], [], []);
     final user = toFirestore(newUser);
-
     await userDb
         .doc(uuid)
         .set(user)
@@ -55,5 +55,22 @@ class UserService {
       print(e);
       return null;
     }
+  }
+
+  static Future<void> updateLikeItem(String uuid, String itemId, bool isLike) async {
+    List<Future> futures;
+    if (isLike) {
+      futures = [
+        userDb.doc(uuid).update({ "likes": FieldValue.arrayUnion([itemId])}),
+        itemDb.doc(itemId).update({ "likes": FieldValue.arrayUnion([uuid])})
+      ];
+    } else {
+      futures = [
+        userDb.doc(uuid).update({ "likes": FieldValue.arrayRemove([itemId])}),
+        itemDb.doc(itemId).update({ "likes": FieldValue.arrayRemove([uuid])})
+      ];
+    }
+
+    await Future.wait(futures);
   }
 }
